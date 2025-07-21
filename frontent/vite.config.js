24f4533b49e6eb2e0d716d8d1400fcc43b974253
Sumanraj Bera <74294151+SumanrajBera@users.noreply.github.com>
2025-07-21 22:51:07 +0530

    Update vite.config.js
    Deployment failed trying again

--- frontent/vite.config.js
+++ frontent/vite.config.js
@@ -4,14 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  base: '/',
   server: {
-    port: 5173,
-  },
-  // 👇 This handles SPA fallback routing for Render or any static host
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  base: './',
+    port: 5173
+  }
 })
